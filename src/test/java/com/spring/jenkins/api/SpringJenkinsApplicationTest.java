6d335751581e3b98e6ea66517a47987323b17e30
Akhila Ayyamgari <akhilagoud50@gmail.com>
2025-03-20 13:37:41 -0600

    change code

--- src/test/java/com/spring/jenkins/api/SpringJenkinsApplicationTest.java
+++ src/test/java/com/spring/jenkins/api/SpringJenkinsApplicationTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringJenkinsApplicationTest {
+public class SpringJenkinsApplicationTest {
 
 	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTest.class);
 
